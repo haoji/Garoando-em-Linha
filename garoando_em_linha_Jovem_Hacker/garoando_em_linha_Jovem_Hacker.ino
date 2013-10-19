3c39b45b3b2e677bf9ef48a81b7d53bbbf21239a
Rafael M. Lopes <rafael@rafaelme.com>
2013-10-19 03:51:04 -0300

    Adicionados sketchs iniciais

---
+++ garoando_em_linha_Jovem_Hacker/garoando_em_linha_Jovem_Hacker.ino
@@ -0,0 +1,60 @@
+// Garoando em Linha - Ver. Jovem Hacker
+// Desenvolvido por Haoji (rafaelme.com)
+
+#include <AFMotor.h>
+#include <Ultrasonic.h>
+#include <IRremote.h>
+
+// Creating ultrasound sensors
+Ultrasonic ultra(A0,A1);
+
+// Creating front motors
+AF_DCMotor front_left(4);
+AF_DCMotor front_right(3);
+
+// Sensor IR
+int RECV_PIN = 16;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup() {
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  Serial.println("Testing Motors");
+
+  // set front motors speed
+  front_left.setSpeed(200);
+  front_right.setSpeed(200);
+  // turn front motors on
+  front_left.run(RELEASE);
+  front_right.run(RELEASE);
+}
+
+void loop() {
+  // sensors
+  Serial.print("Sensor de ultrasom: ");
+  Serial.print(ultra.Ranging(CM)); // CM ou INC (polegadas)
+  Serial.print(" cm     " );
+  Serial.println();
+  delay(1000);
+  
+  if (irrecv.decode(&results)) {
+    front_right.run(FORWARD);
+    front_left.run(FORWARD);
+    delay(1000);
+    //Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
+  
+  if (ultra.Ranging(CM) < 10) {
+  // mover para tras
+  front_right.run(BACKWARD);
+  front_left.run(BACKWARD);
+  delay(1000);
+  }
+  // parar
+  front_right.run(RELEASE);
+  front_left.run(RELEASE);
+}
